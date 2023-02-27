d810ecc70c51825b3030adaab18586af3ff51aa1
Catriona Ferguson <114424301+cferg97@users.noreply.github.com>
2023-02-27 19:32:00 +0000

    add initial files

---
+++ mtgmarketplace-fe/src/App.js
@@ -0,0 +1,13 @@
+import { Container, Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <>
+      <Container fluid className="m-0 p-0" style={{ width: "100%", height: "100%" }}>
+        
+      </Container>
+    </>
+  );
+}
+
+export default App;
